fe3bc84369cf1cb9a464aab2f8618b90f76dd3ba
Alejandro Bogado <wentapps.co@gmail.com>
2019-07-24 20:34:29 -0300

    Cambios para que compile

--- src/app/pages/slides/slides.page.ts
+++ src/app/pages/slides/slides.page.ts
@@ -10,19 +10,19 @@ export class SlidesPage implements OnInit {
 
     slides: { img: string, titulo: string, desc: string }[] = [
         {
-          img:'/assets/assets/slides/productos-todos.svg',
+          img:'/assets/assets/slides/008-shop.svg',
           titulo: 'Busca los mejores precios',
-          desc: '',
+          desc: 'En los locales seleccionados',
         },
         {
-          img:'/assets/assets/slides/productos-listas.svg',
+          img:'/assets/assets/slides/003-shopping-list.svg',
           titulo: 'Organiza tu lista',
-          desc: '',
+          desc: 'Agrega y elimina, organizala como vos quieras.',
         },
         {
-          img:'/assets/assets/slides/productos-codigo.svg',
+          img:'/assets/assets/slides/013-barcode-1.svg',
         titulo: 'Escanea códigos de barra',
-        desc: '',
+        desc: 'Escanea todos los productos seleccionados',
         },
     ];
 
